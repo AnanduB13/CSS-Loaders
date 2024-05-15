59a813ea73a394238dffc1d22d7234e067bcda96
Anandu BFR <116485917+AnanduB13@users.noreply.github.com>
2024-05-15 17:23:29 +0530

    Add files via upload

---
+++ Loader/TypeWritter Loader/script.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", function() {
+    var loader = document.querySelector(".loader");
+    setTimeout(function() {
+      loader.classList.add("loaded");
+    }, 2000);
+  });
+  
+  
